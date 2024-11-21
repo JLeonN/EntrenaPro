1c4f1f132f49489039d1e7a9d493ddb2cec663ef
Leonardo Núñez <124943644+JLeonN@users.noreply.github.com>
2024-11-21 06:51:43 -0300

    Pantalla de inicio

--- src/Componentes/Inicio/Inicio.jsx
+++ src/Componentes/Inicio/Inicio.jsx
@@ -2,10 +2,38 @@ import React from 'react'
 
 function Inicio() {
   return (
-    <>
-    <h1>hola</h1>
-    </>
-  )
+    <div className="contenedorPrincipal">
+      <div className="encabezado">
+        <img
+          src="./EntrenaPro/Imagenes/Iconos/Estrella.jpg" // Cambiar por ícono que va
+          alt="Logo"
+          className="iconoLogo"
+        />
+        <h1 className="titulo">ENTRENA PRO</h1>
+      </div>
+      <form className="formulario">
+        <div className="campo">
+          <input
+            type="text"
+            placeholder="Usuario"
+            className="inputCampo"
+          />
+        </div>
+        <div className="campo">
+          <input
+            type="password"
+            placeholder="Contraseña"
+            className="inputCampo"
+          />
+        </div>
+      </form>
+      <div className="botones">
+        <button className="boton">INICIAR</button>
+        <button className="boton">REGISTRARSE</button>
+        <button className="boton">CONTINUAR</button>
+      </div>
+    </div>
+  );
 }
 
-export default Inicio
+export default Inicio;
