c26bd699475b7b1d4d346fd181cba67eff98ee92
Leonardo Núñez <124943644+JLeonN@users.noreply.github.com>
2024-11-26 07:18:21 -0300

    Preparando la ventana modal

--- src/Componentes/Rutinas/ListaRutinas.jsx
+++ src/Componentes/Rutinas/ListaRutinas.jsx
@@ -1,24 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import ForRutnas from "../Formularios/Rutinas/ForRutnas";
 
 function Inicio() {
   return (
-    <div className="contenedorPrincipal">
-      <div className="encabezadoDos">
-        <img
-          src="./Imagenes/Iconos/Estrella.jpg" // Cambiar por ícono que va
-          alt="Logo"
-          className="iconoLogo"
-        />
-        <h1 className="titulo">ENTRENA PRO</h1>
-        <h3 className="subTitulo">Rutinas</h3>
-      </div>
-      <div className="botones">
-        <Link to="/RutinasModal">
+    <>
+      <div className="contenedorPrincipal">
+        <div className="encabezadoDos">
+          <img
+            src="./EntrenaPro/Imagenes/Iconos/Estrella.jpg" // Cambiar por ícono que va
+            alt="Logo"
+            className="iconoLogo"
+          />
+          <h1 className="titulo">ENTRENA PRO</h1>
+          <h3 className="subTitulo">Rutinas</h3>
+        </div>
+        <div className="botones">
+          {/* <Link to="/RutinasModal"> */}
           <button className="boton">Crea tu rutina</button>
-        </Link>
+          {/* </Link> */}
+        </div>
       </div>
-    </div>
+      <ForRutnas />
+    </>
   );
 }
 
